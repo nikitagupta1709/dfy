2ce2fadb3a9fb553041a3473664f030702d136bd
Nikita Gupta <nikitagupta1709@gmail.com>
2023-01-18 17:18:07 +0530

    done

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <HelloWorld msg="Hey party on!" otherMSg=" Let the party begun!!"/>
+  </div>
+</template>
+
+<script setup>
+  import HelloWorld from "./components/HelloWorld.vue"
+  console.log("Hello World:", HelloWorld);
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+
+<template>
+    <div>
+      <h1>{{ msg }}</h1>
+
+      <h6>{{ otherMSg }}</h6>
+    </div>
+  </template>
+  
+  <script setup>
+  import { defineProps } from "vue";
+    defineProps(["msg", "otherMSg"]);
+  </script>
+  
+  <style scoped></style>
+  
